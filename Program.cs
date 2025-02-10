003ed82179b50d0fefac717ccd1ee3afc1f849d4
Sarah Tiffany <362575441@GoTVDSB.ca>
2025-02-10 14:43:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,282 @@
+﻿namespace Topic_4___Assignment_Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) 
+
+
+            //string user_first_name, user_last_name, month, city, province_state, country;
+
+            //int user_age, year, date_number_1_31, fare_class;
+
+            //double user_salary;
+
+            //Console.WriteLine("Hello fair User! Welcome aboard the Elmington Express!");
+            //Console.WriteLine("Please enter your first and last name to get your boarding number!");
+            //Console.WriteLine();
+            //Console.WriteLine("Last Name: ");
+            //user_last_name = Console.ReadLine();
+            //Console.WriteLine("First Name: ");
+            //user_first_name = Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Welcome aboard " + user_first_name + " " + user_last_name + "! Your boarding number is #004703");
+            //Console.WriteLine("Before you board, we need to ask you a couple of routine questions to verify our file.");
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("Where is your desired destination of travel? Please capitalize.");
+            //Console.WriteLine();
+            //Console.WriteLine("City / Town: ");
+            //city = Console.ReadLine();
+            //Console.WriteLine("Province / State: ");
+            //province_state = Console.ReadLine();
+            //Console.WriteLine("Country: ");
+            //country = Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Thank you very much!");
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("What is your requested date of travel? Please follow the directions below:");
+            //Console.WriteLine("Date Number between 1 and 31 (Do not include day name: ie monday, month or year): ");
+
+            //Int32.TryParse(Console.ReadLine(), out date_number_1_31);
+            //Console.WriteLine("Name of the Month");
+            //month = Console.ReadLine();
+            //Console.WriteLine("What is the desired year of travel?");
+            //Int32.TryParse(Console.ReadLine(), out year);
+
+            //Console.WriteLine();
+            //Console.WriteLine("What is your age as of 2025?");
+            //Int32.TryParse(Console.ReadLine(), out user_age);
+            //Console.WriteLine("Wow you are actually " + user_age + "! People look very young these days.");
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("What is your desired fare? Type '1' for Business, '2' for Economy or any number for Escape: ");
+            //Int32.TryParse(Console.ReadLine(), out fare_class);
+
+            //Console.WriteLine("Finally, what was your yearly salary as of last year? We need to make sure you can afford this fare.");
+            //Console.WriteLine("Do not include commas or spaces");
+
+            //Double.TryParse(Console.ReadLine(), out user_salary);
+            //Console.WriteLine(user_salary.ToString("C") + " eh? Not bad! I see no reason why you cannot board the train.");
+            //Console.WriteLine();
+            //Console.WriteLine("Have a lovely trip!");
+            //Console.WriteLine("Here is your boarding pass:");
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine("========================================================");
+            //Console.WriteLine("User Details # 004703");
+            //Console.WriteLine();
+            //Console.WriteLine("Register Name     : " + user_last_name + ", " + user_first_name);
+            //Console.WriteLine("Register Age      : " + user_age);
+            //Console.WriteLine("Register Salary   : " + user_salary.ToString("C"));
+            //Console.WriteLine();
+            //Console.WriteLine("========================================================");
+            //Console.WriteLine("Travel Details");
+            //Console.WriteLine();
+            //Console.WriteLine("Date of Departure : " + date_number_1_31 + ", " + month + ", " + year);
+            //Console.WriteLine("Destination       : " + city + ", " + province_state + ", " + country);
+            //if (country == "Canada")
+            //    Console.WriteLine("International     : No");
+            //else Console.WriteLine("International     : Yes");
+
+            //if (fare_class == 1)
+            //    Console.WriteLine("Class Fare        : Business Class");
+            //else if (fare_class == 2)
+            //    Console.WriteLine("Class Fare        : Economy Class");
+            //else Console.WriteLine("Class Fare       : Escape Class");
+            //Console.WriteLine();
+            //Console.WriteLine("========================================================");
+
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            ////2
+
+            //string register_given_name,
+            //    register_last_name, course_code_homeroom, attending_school,
+            //    street_name,
+            //    address_city,
+            //    address_province,
+            //    address_country,
+            //    user_email,
+            //    application_username,
+            //    application_passcode;
+
+            //int grade_9_12,
+            //    student_id,
+            //    street_number;
+
+            //double student_average;
+
+
+            //Console.WriteLine("Please Enter Your Information to Access the Board's Resource Databases");
+            //Console.WriteLine();
+            //Console.WriteLine("General Information: ");
+            //Console.WriteLine();
+            //Console.WriteLine("Enter Your Last Name:");
+            //register_last_name = Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Enter Your First Name:");
+            //register_given_name = Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Enter Your School Email: ");
+            //user_email = Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Enter Your Set Password:");
+            //application_passcode = Console.ReadLine();
+            //Console.WriteLine();
+
+
+            //Console.WriteLine("School Information:");
+            //Console.WriteLine();
+            //Console.WriteLine("Enter Your 9 Digit Student ID:");
+            //Int32.TryParse(Console.ReadLine(), out student_id);
+            //Console.WriteLine("Enter Your Grade Number (9 - 12):");
+            //Int32.TryParse(Console.ReadLine(), out grade_9_12);
+            //Console.WriteLine("Enter Your Overall Average (Do not include %)");
+            //Double.TryParse(Console.ReadLine(), out student_average);
+            //student_average = Math.Round(student_average, 2);
+            //Console.WriteLine("Enter Your Current Attending School:");
+            //attending_school = Console.ReadLine();
+            //Console.WriteLine("Enter Your Homeroom Course Code:");
+            //course_code_homeroom = Console.ReadLine();
+
+
+            //Console.WriteLine();
+            //Console.WriteLine("Enter Your Address:");
+            //Console.WriteLine();
+            //Console.WriteLine("Street Number:");
+            //Int32.TryParse(Console.ReadLine(), out street_number);
+            //Console.WriteLine("Street Name:");
+            //street_name = Console.ReadLine();
+            //Console.WriteLine("City / Town:");
+            //address_city = Console.ReadLine();
+            //Console.WriteLine("Province / State:");
+            //address_province = Console.ReadLine();
+            //Console.WriteLine("Country:");
+            //address_country = Console.ReadLine();
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine("========================================================");
+            //Console.WriteLine("Your Entered Information");
+            //Console.WriteLine("General Identification");
+            //Console.WriteLine();
+            //Console.WriteLine("Name \t\t\t" + ": " + register_last_name + ", " + register_given_name);
+            //Console.WriteLine("Email \t\t\t" + ": " + user_email);
+            //Console.WriteLine("Password \t\t" + ": " + application_passcode);
+            //Console.WriteLine();
+            //Console.WriteLine("========================================================");
+            //Console.WriteLine("School Identification");
+            //Console.WriteLine();
+            //Console.WriteLine("Student ID \t\t" + ": " + student_id);
+            //Console.WriteLine("Grade (9-12) \t\t" + ": " + grade_9_12);
+            //Console.WriteLine("Student Average \t" + ": " + student_average + " %");
+            //Console.WriteLine("Attending School \t" + ": " + attending_school);
+            //Console.WriteLine("Homeroom Course Code \t" + ": " + course_code_homeroom);
+            //Console.WriteLine();
+            //Console.WriteLine("========================================================");
+            //Console.WriteLine("Home Address \t\t" + ": " + street_number + ", " + street_name);
+            //Console.WriteLine("\t\t\t" + "  " + address_city + ", " + address_province + ", " + address_country);
+            //Console.WriteLine();
+            //Console.WriteLine("========================================================");
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            ////3)
+
+            //string age_calculator_name;
+
+            //int age_calculator_value;
+
+            //Console.WriteLine("~~~ Welcome to the Age Name Number Game ~~~");
+            //Console.WriteLine("Please enter your name to find out how old you were 5 years ago,");
+            //Console.WriteLine("and how old you'll be in 5 more years with ease!");
+
+            //age_calculator_name = Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Nice to meet you, " + (age_calculator_name) + "!");
+            //Console.WriteLine("How old are you!");
+            //Int32.TryParse(Console.ReadLine(), out age_calculator_value);
+            //Console.WriteLine(age_calculator_name + ", 5 years ago, you were " + (age_calculator_value - 5) + ".");
+            //Console.WriteLine("And in five more years, you will be " + (age_calculator_value + 5) + ".");
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+
+            //4)
+
+            //double first_number, second_number, third_number;
+
+            //Console.WriteLine("Please Enter a Number!");
+            //Double.TryParse((Console.ReadLine()), out first_number);
+            //Console.WriteLine("Please Enter Another Number!");
+            //Double.TryParse((Console.ReadLine()), out second_number);
+            //Console.WriteLine("Please Enter a Third Number!");
+            //Double.TryParse((Console.ReadLine()), out third_number);
+
+            //Console.WriteLine("The sum of the numbers divided by two is as follows:");
+            //Console.WriteLine((first_number + second_number + third_number) / 2+ ".");
+
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //5) 
+
+            string grocery_name_one,
+                grocery_name_two;
+
+            double grocery_price_one,
+                grocery_price_two,
+                pretotal_grocery_price,
+                subtotal_grocery,
+                final_price;
+            
+
+            Console.WriteLine("Welcome to Foodland! Canada's Favourite Grocery Chain!");
+            Console.WriteLine("You have 2 item(s) in the cart");
+            Console.WriteLine("Please enter the name of the first item.");
+            grocery_name_one = Console.ReadLine();
+            Console.WriteLine("Please enter the price of the first item. (Do not include $)");
+            Double.TryParse((Console.ReadLine()), out grocery_price_one);
+            Console.WriteLine("Please enter the name of the second item.");
+            grocery_name_two = Console.ReadLine();
+            Console.WriteLine("Please enter the price of the second item. (Do not include $)");
+            Double.TryParse((Console.ReadLine()), out grocery_price_two);
+            Console.WriteLine("Congratulations! Your items have a 20% Discound.");
+            Console.WriteLine();
+            Console.WriteLine("Below is your sales recipt:");
+            Console.WriteLine();
+
+            pretotal_grocery_price = (grocery_price_one + grocery_price_two);
+            subtotal_grocery = Math.Round(((pretotal_grocery_price - (pretotal_grocery_price * 0.20))), 2);
+            Console.WriteLine();
+            Console.WriteLine("Sales Recipt");
+            Console.WriteLine("===================================");
+            Console.WriteLine("Item One  :" + "\t" + " " + grocery_name_one);                
+            Console.WriteLine("Price     :" + "\t" + " " + (Math.Round(grocery_price_one, 2).ToString("C")));
+            Console.WriteLine("Item Two  :" + "\t" + " " + grocery_name_two);
+            Console.WriteLine("Price     :" + "\t" + " " + (Math.Round(grocery_price_two, 2).ToString("C")));
+            Console.WriteLine("===================================");
+            Console.WriteLine("Total     :" + "\t" + " " + pretotal_grocery_price.ToString("C"));
+            Console.WriteLine("Discount  :" + "\t" + "-" + (Math.Round(((pretotal_grocery_price) * 0.20), 2)).ToString("C"));
+            Console.WriteLine("Subtotal  :" + "\t" + " " + subtotal_grocery);
+            Console.WriteLine("Taxes     :" + "\t" + " " + (Math.Round((subtotal_grocery * 0.13), 2).ToString("C")));
+            Console.WriteLine("===================================");
+            Console.WriteLine("Total Owed:" + "\t" + " " + (Math.Round((subtotal_grocery * 1.13), 2).ToString("C")));
+            Console.ReadLine();
+
+        }
+    }
+}
